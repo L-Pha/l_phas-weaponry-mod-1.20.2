fea8fd02083c904b0b86a270301ce40645573779
L-Pha <fenris.lpha@gmail.com>
2024-04-20 11:54:10 +0630

    -Added Iron Stick! -Changed Iron Blob texture!

--- src/main/java/net/lpha/lphas_weaponry_mod/item/ModItems.java
+++ src/main/java/net/lpha/lphas_weaponry_mod/item/ModItems.java
@@ -1,12 +1,10 @@
 package net.lpha.lphas_weaponry_mod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.lpha.lphas_weaponry_mod.L_PhasWeaponryMod;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -16,9 +14,14 @@ import net.minecraft.util.Identifier;
 public class ModItems {
 
     public static final Item IRON_BLOB = registerItem("iron_blob", new Item(new FabricItemSettings()));
+    public static final Item IRON_STICK = registerItem("iron_stick", new Item(new FabricItemSettings()));
+
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries){
+
         entries.add(IRON_BLOB);
+        entries.add(IRON_STICK);
+
     }
 
     private static Item registerItem(String name, Item item){
